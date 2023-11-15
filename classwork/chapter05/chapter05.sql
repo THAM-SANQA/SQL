36a91c3be4de2e66d004d68efdf4b91511d50937
THAM-SANQA <ndabat020@gmail.com>
2023-11-15 08:54:20 +0200

    chapter 5 TRY IT YOURSELF

--- classwork/chapter05/chapter05.sql
+++ classwork/chapter05/chapter05.sql
@@ -187,3 +187,36 @@ SELECT mode() WITHIN GROUP (ORDER BY p0010001)
 FROM us_counties_2010;
 
 
+--------------------------------------------------------------
+-- Try it yourself
+--------------------------------------------------------------
+
+--------------------------------------------------------------
+-- Chapter 5: Basic Math and Stats with SQL
+--------------------------------------------------------------
+
+-- QUESTION 1:
+SELECT 3.14 * 5 ^ 2;
+-- The result is an area of 78.5 square inches.
+
+-- QUESTION 2:
+SELECT geo_name,
+       state_us_abbreviation,
+       p0010001 AS total_population,
+       p0010005 AS american_indian_alaska_native_alone,
+       (CAST (p0010005 AS numeric(8,1)) / p0010001) * 100
+           AS percent_american_indian_alaska_native_alone
+FROM us_counties_2010
+WHERE state_us_abbreviation = 'NY'
+ORDER BY percent_american_indian_alaska_native_alone DESC;
+-- Franklin County, N.Y., with 7.4%. The county contains the St. Regis Mohawk
+
+-- QUESTION 3: 
+SELECT state_us_abbreviation,
+       percentile_cont(0.5)
+          WITHIN GROUP (ORDER BY p0010001) AS median
+FROM us_counties_2010
+WHERE state_us_abbreviation IN ('NY', 'CA')
+GROUP BY state_us_abbreviation;
+-- Carlifonia had the higher median population.
+
