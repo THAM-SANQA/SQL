ceb0ec5fc49e1be99df88a1be2ea66c6451a360b
THAM-SANQA <ndabat020@gmail.com>
2023-11-15 09:33:07 +0200

    chapter 6 TRY IT YOURSELF

--- classwork/chapter06/chapter06.sql
+++ classwork/chapter06/chapter06.sql
@@ -179,4 +179,52 @@ ORDER BY pct_change DESC;
 --------------------------------------------------------------
 -- Try it yourself
 --------------------------------------------------------------
+-- QUESTION 1: 
+-- Counties that exist in 2010 data but not 2000 include five county equivalents
+-- in Alaska (called boroughs) plus Broomfield County, Colorado.
+
+SELECT c2010.geo_name,
+       c2010.state_us_abbreviation,
+       c2000.geo_name
+FROM us_counties_2010 c2010 LEFT JOIN us_counties_2000 c2000
+ON c2010.state_fips = c2000.state_fips
+   AND c2010.county_fips = c2000.county_fips
+WHERE c2000.geo_name IS NULL;
+
+-- Counties that exist in 2000 data but not 2010 include three county
+-- equivalents in Alaska (called boroughs) plus Clifton Forge city, Virginia.
+
+SELECT c2010.geo_name,
+       c2000.geo_name,
+       c2000.state_us_abbreviation
+FROM us_counties_2010 c2010 RIGHT JOIN us_counties_2000 c2000
+ON c2010.state_fips = c2000.state_fips
+   AND c2010.county_fips = c2000.county_fips
+WHERE c2010.geo_name IS NULL;
+
+-- QUESTION 2: 
+-- 3.2% is the median percent change
+SELECT percentile_cont(.5)
+       WITHIN GROUP (ORDER BY round( (CAST(c2010.p0010001 AS numeric(8,1)) - c2000.p0010001)
+           / c2000.p0010001 * 100, 1 )) AS percentile_50th
+FROM us_counties_2010 c2010 INNER JOIN us_counties_2000 c2000
+ON c2010.state_fips = c2000.state_fips
+   AND c2010.county_fips = c2000.county_fips;
+
+-- QUESTION 3:
+-- Answer: St. Bernard Parish, La. It and other Louisiana parishes (the county
+-- equivalent name in Louisiana) experienced substantial population loss
+-- following Hurricane Katrina in 2005.
+
+SELECT c2010.geo_name,
+       c2010.state_us_abbreviation,
+       c2010.p0010001 AS pop_2010,
+       c2000.p0010001 AS pop_2000,
+       c2010.p0010001 - c2000.p0010001 AS raw_change,
+       round( (CAST(c2010.p0010001 AS DECIMAL(8,1)) - c2000.p0010001)
+           / c2000.p0010001 * 100, 1 ) AS pct_change
+FROM us_counties_2010 c2010 INNER JOIN us_counties_2000 c2000
+ON c2010.state_fips = c2000.state_fips
+   AND c2010.county_fips = c2000.county_fips
+ORDER BY pct_change ASC;
 
