11b98349ff89df71e5f5a3f5ecadb0ae108f1ed6
THAM-SANQA <ndabat020@gmail.com>
2023-11-21 09:16:39 +0200

    TRY IT YOURSELF CHAPTER 7

--- classwork/chapter07/chapter07.sql
+++ classwork/chapter07/chapter07.sql
@@ -24,7 +24,7 @@ VALUES ('T229901', 'Lynn', 'Malero');
 INSERT INTO natural_key_example (license_id, first_name, last_name)
 VALUES ('T229901', 'Sam', 'Tracy');
 
--- Listing 7-3: Declaring a composite primary key as a natural key
+-- Declaring a composite primary key as a natural key
 CREATE TABLE natural_key_composite_example (
     student_id varchar(10),
     school_day date,
@@ -149,7 +149,7 @@ CREATE TABLE new_york_addresses (
 );
 
 COPY new_york_addresses
-FROM 'C:\YourDirectory\city_of_new_york.csv'
+FROM 'C:\Users\user1\Documents\CodeCollegeZA\BootCamp_2023\SQL\classwork\chapter07\city_of_new_york.csv'
 WITH (FORMAT CSV, HEADER);
 
 -- Benchmark queries for index performance
@@ -164,3 +164,92 @@ WHERE street = 'ZWICKY AVENUE';
 
 -- Creating a B-Tree index on the new_york_addresses table
 CREATE INDEX street_idx ON new_york_addresses (street);
+
+
+-- Consider the following two tables from a database you’re making to keep
+-- track of your vinyl LP collection. Start by reviewing these CREATE TABLE
+-- statements.
+
+-- The albums table includes information specific to the overall collection
+-- of songs on the disc. The songs table catalogs each track on the album.
+-- Each song has a title and its own artist column, because each song might.
+-- feature its own collection of artists.
+
+CREATE TABLE albums (
+    album_id bigserial,
+    album_catalog_code varchar(100),
+    album_title text,
+    album_artist text,
+    album_time interval,
+    album_release_date date,
+    album_genre varchar(40),
+    album_description text
+);
+
+CREATE TABLE songs (
+    song_id bigserial,
+    song_title text,
+    song_artist text,
+    album_id bigint
+);
+
+--------------------------------------------------------------
+-- Try it yourself
+--------------------------------------------------------------
+-- QUESTION 1.
+
+CREATE TABLE albums (
+    album_id bigserial,
+    album_catalog_code varchar(100) NOT NULL,
+    album_title text NOT NULL,
+    album_artist text NOT NULL,
+    album_release_date date,
+    album_genre varchar(40),
+    album_description text,
+    CONSTRAINT album_id_key PRIMARY KEY (album_id),
+    CONSTRAINT release_date_check CHECK (album_release_date > '1/1/1925')
+);
+
+CREATE TABLE songs (
+    song_id bigserial,
+    song_title text NOT NULL,
+    song_artist text NOT NULL,
+    album_id bigint REFERENCES albums (album_id),
+    CONSTRAINT song_id_key PRIMARY KEY (song_id)
+);
+
+-- Answers:
+-- a) Both tables get a primary key using surrogate key id values that are
+-- auto-generated via serial data types.
+
+-- b) The songs table references albums via a foreign key constraint.
+
+-- c) In both tables, the title and artist columns cannot be empty, which
+-- is specified via a NOT NULL constraint. We assume that every album and
+-- song should at minimum have that information.
+
+-- d) In albums, the album_release_date column has a CHECK constraint
+-- because it would be likely impossible for us to own an LP made before 1925.
+
+
+-- QUESTION 2. Instead of using album_id as a surrogate key for your primary key, are
+-- there any columns in albums that could be useful as a natural key? What would
+-- you have to know to decide?
+
+-- Answer:
+-- We could consider the album_catalog_code. We would have to answer yes to
+-- these questions:
+-- - Is it going to be unique across all albums released by all companies?
+-- - Will we always have one?
+
+
+-- QUESTION 3. To speed up queries, which columns are good candidates for indexes?
+
+-- Answer:
+-- Primary key columns get indexes by default, but we should add an index
+-- to the album_id foreign key column in the songs table because we'll use
+-- it in table joins. It's likely that we'll query these tables to search
+-- by titles and artists, so those columns in both tables should get indexes
+-- too. The album_release_date in albums also is a candidate if we expect
+-- to perform many queries that include date ranges.
+
