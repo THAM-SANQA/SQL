5c7b40fff3f189953d68e8ca1e619bb965ddcae7
THAM-SANQA <ndabat020@gmail.com>
2023-11-25 17:47:33 +0200

    TRY IT YOURSELF

--- classwork/chapter11/chapter11.sql
+++ classwork/chapter11/chapter11.sql
@@ -208,3 +208,49 @@ SELECT segment,
 FROM train_rides;
 
 
+--------------------------------------------------------------
+-- Try it yourself
+--------------------------------------------------------------
+-- QUESTION 1.
+-- The longest trips are almost 24 hours and the shortest trips are less 
+-- than zero minutes, there could be a timestap error when capturing the data.
+SELECT
+    trip_id,
+    tpep_pickup_datetime,
+    tpep_dropoff_datetime,
+    tpep_dropoff_datetime - tpep_pickup_datetime AS length_of_ride
+FROM nyc_yellow_taxi_trips_2016_06_01
+ORDER BY length_of_ride;
+
+-- QUESTION 2. 
+SELECT '2100-01-01 00:00:00-05' AT TIME ZONE 'US/Eastern' AS new_york,
+       '2100-01-01 00:00:00-05' AT TIME ZONE 'Europe/London' AS london,
+       '2100-01-01 00:00:00-05' AT TIME ZONE 'Africa/Johannesburg' AS johannesburg,
+       '2100-01-01 00:00:00-05' AT TIME ZONE 'Europe/Moscow' AS moscow,
+       '2100-01-01 00:00:00-05' AT TIME ZONE 'Australia/Melbourne' AS melbourne;
+
+-- QUESTION 3. 
+SELECT
+    round(
+          corr(total_amount, (
+              date_part('epoch', tpep_dropoff_datetime) -
+              date_part('epoch', tpep_pickup_datetime)
+                ))::numeric, 2
+          ) AS amount_time_corr,
+    round(
+        regr_r2(total_amount, (
+              date_part('epoch', tpep_dropoff_datetime) -
+              date_part('epoch', tpep_pickup_datetime)
+        ))::numeric, 2
+    ) AS amount_time_r2,
+    round(
+          corr(total_amount, trip_distance)::numeric, 2
+          ) AS amount_distance_corr,
+    round(
+        regr_r2(total_amount, trip_distance)::numeric, 2
+    ) AS amount_distance_r2
+FROM nyc_yellow_taxi_trips_2016_06_01
+WHERE tpep_dropoff_datetime - tpep_pickup_datetime <= '3 hours'::interval;
+
+-- Cost of a taxi ride is based on both time and distance.
+
