1522eea566b916588c4d2c32a31098d5916e12fd
THAM-SANQA <ndabat020@gmail.com>
2023-12-01 18:18:11 +0200

    chapter 17

--- classwork/chapter17/chapter17.sql
+++ classwork/chapter17/chapter17.sql
@@ -1,20 +1,10 @@
---------------------------------------------------------------
--- Practical SQL: A Beginner's Guide to Storytelling with Data
--- by Anthony DeBarros
-
--- Chapter 17 Code Examples
---------------------------------------------------------------
-
 -- VACUUM
-
--- Listing 17-1: Creating a table to test vacuuming
-
+-- Creating a table to test vacuuming
 CREATE TABLE vacuum_test (
     integer_column integer
 );
 
--- Listing 17-2: Determining the size of vacuum_test
-
+-- Determining the size of vacuum_test
 SELECT pg_size_pretty(
            pg_total_relation_size('vacuum_test')
        );
@@ -24,8 +14,7 @@ SELECT pg_size_pretty(
            pg_database_size('analysis')
        );
 
--- Listing 17-3: Inserting 500,000 rows into vacuum_test
-
+-- Inserting 500,000 rows into vacuum_test
 INSERT INTO vacuum_test
 SELECT * FROM generate_series(1,500000);
 
@@ -34,8 +23,7 @@ SELECT pg_size_pretty(
            pg_table_size('vacuum_test')
        );
 
--- Listing 17-4: Updating all rows in vacuum_test
-
+-- Updating all rows in vacuum_test
 UPDATE vacuum_test
 SET integer_column = integer_column + 1;
 
@@ -44,8 +32,7 @@ SELECT pg_size_pretty(
            pg_table_size('vacuum_test')
        );
 
--- Listing 17-5: Viewing autovacuum statistics for vacuum_test
-
+-- Viewing autovacuum statistics for vacuum_test
 SELECT relname,
        last_vacuum,
        last_autovacuum,
@@ -59,16 +46,14 @@ SELECT *
 FROM pg_stat_all_tables
 WHERE relname = 'vacuum_test';
 
--- Listing 17-6: Running VACUUM manually
-
+-- Running VACUUM manually
 VACUUM vacuum_test;
 
 VACUUM; -- vacuums the whole database
 
 VACUUM VERBOSE; -- provides messages
 
--- Listing 17-7: Using VACUUM FULL to reclaim disk space
-
+-- Using VACUUM FULL to reclaim disk space
 VACUUM FULL vacuum_test;
 
 -- Test its size again
@@ -77,28 +62,25 @@ SELECT pg_size_pretty(
        );
        
 -- SETTINGS
-
--- Listing 17-8: Showing the location of postgresql.conf
-
+-- Showing the location of postgresql.conf
 SHOW config_file;
 
--- Listing 17-10: Show the location of the data directory
-
+-- Show the location of the data directory
 SHOW data_directory;
 
 -- reload settings
 -- Mac and Linux: pg_ctl reload -D '/path/to/data/directory/'
 -- Windows: pg_ctl reload -D "C:\path\to\data\directory\"
 
-
+-- pg_dump: error: connection to server at "localhost" (::1), port 5432 failed: FATAL:  password authentication failed for user "thami82d"
 -- BACKUP AND RESTORE
 
--- Listing 17-11: Backing up the analysis database with pg_dump
-pg_dump -d analysis -U [user_name] -Fc > analysis_backup.sql
+-- Backing up the analysis database with pg_dump
+pg_dump -d analysis -U thami82d -Fc > analysis_backup.sql
 
 -- Back up just a table
 pg_dump -t 'train_rides' -d analysis -U [user_name] -Fc > train_backup.sql 
 
--- Listing 17-12: Restoring the analysis database with pg_restore
+-- Restoring the analysis database with pg_restore
 
 pg_restore -C -d postgres -U postgres analysis_backup_custom.sql
